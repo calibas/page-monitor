380cd62780e47c4d743807944fd5b6286affc648
Will Harader <wharader@gmail.com>
2019-11-12 21:18:37 -0800

    .env implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #[macro_use]
 extern crate diesel;
+#[macro_use]
+extern crate dotenv_codegen;
 
 pub mod schema;
 pub mod models;
@@ -8,7 +10,8 @@ use diesel::prelude::*;
 use diesel::mysql::MysqlConnection;
 
 pub fn establish_connection() -> MysqlConnection {
-    let database_url = "mysql://user:password@localhost:3306/page_monitor";
+    println!("{}", dotenv!("DATABASE_URL"));
+    let database_url = dotenv!("DATABASE_URL");
 
     MysqlConnection::establish(&database_url)
         .expect(&format!("Error connecting to {}", database_url))
